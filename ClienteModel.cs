d0647a2ddbe63e45a8d8f930a378d74f3cc63fe7
Eric Lopes de Medeiros <145200474+Ericao327@users.noreply.github.com>
2024-04-20 11:15:25 -0300

    Add files via upload

---
+++ ClienteModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppUsuario.Models
+{
+	[Table("Cliente")]
+	public class ClienteModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[MaxLength(60)]
+		public string nome { get; set;}
+		[MaxLength(80)]
+		public string Email { get; set;}
+		[MaxLength(80)]
+		public string Celular { get; set;}
+		[MaxLength(11)]
+		public string Cidade { get; set;}
+		public bool Administrador { get; set;}
+		public bool Ativo { get ; set;}
+		public DateTime DataDoCadastro { get; set;}
+		public DateTime DataDaIniciativa { get; set;}
+
+	}
+}
---
+++ UsuarioModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppUsuario.Models
+{
+	[Table("Usuario")]
+	public class UsuarioModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[MaxLength(60)]
+		public string nome { get; set; }
+		[MaxLength(80)]
+		public string Email { get; set; }
+		[MaxLength(10, ErrorMessage = "0 tamanho máximo é {1} caracteres!")]
+		[MinLength(5, ErrorMessage = "0 tamanho mínimo é {1} caracteres!")]
+		public string Senha { get; set; }
+		public bool Administrador { get; set; }
+		public bool Ativo { get ; set; }
+		public DateTime DataDoCadastro { get; set; }
+		public DateTime DataDaIniciativa { get; set;}
+	}
+}
